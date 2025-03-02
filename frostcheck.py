b9f4e63dccca148f1e7d69ca1dfc44c2f2e260c4
mm042676 <mm042676@gmail.com>
2025-03-02 15:03:00 -0500

    initial commit, script is working

---
+++ frostcheck.py
@@ -0,0 +1,46 @@
+
+# first, we need to import the necessary libraries
+
+import requests
+import os
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+
+env_path = Path('.') / '.env'
+# load_dotenv(dotenv_path=env_path)
+load_dotenv()
+
+# define variables
+
+weatherAPIUrl = "https://api.weather.gov/gridpoints/ILN/39,55/forecast"
+coldThreshold = 40
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# define functions
+
+def getWeatherData():
+    response = requests.get(weatherAPIUrl)
+    return response.json()
+
+def getTonightsLowTemp(weatherData):
+    lowTemp = 70
+    for period in weatherData['properties']['periods']:
+        if "Tonight" in period['name']:
+            lowTemp = period['temperature']
+    return lowTemp
+
+def sendTextMessage(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}"
+    print(requests.get(url).json())
+
+# main program
+
+weatherData = getWeatherData()
+tonightsLowTemp = getTonightsLowTemp(weatherData)
+print(f"Tonight's low temperature is {tonightsLowTemp} degrees.")
+
+if tonightsLowTemp < coldThreshold:
+    sendTextMessage("Tonights low is " + str(tonightsLowTemp) + ".  It's going to be too cold tonight! Bring your plants inside!")
+else:
+    print("It's not going to be too cold tonight.")
